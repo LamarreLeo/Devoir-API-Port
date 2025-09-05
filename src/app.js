e9826eabc38f4894081e0a87d0d78feb0c40a311
Leo.Lamarre <leo.lamarre24@gmail.com>
2025-09-06 01:35:26 +0200

    Création du fichier de configuration de base de données

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
+
+connectDB();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
