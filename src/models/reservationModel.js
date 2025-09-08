1c504f8c2bea22564de4a48e33c98418238b3244
Leo.Lamarre <leo.lamarre24@gmail.com>
2025-09-08 19:25:26 +0200

    Documentation JSDoc de reservationModel.js

--- src/models/reservationModel.js
+++ src/models/reservationModel.js
@@ -1,5 +1,23 @@
+/**
+ * Modèle de réservation pour l'application.
+ * @module models/reservationModel
+ * @requires mongoose
+ */
+
 const mongoose = require("mongoose");
 
+/**
+ * Schéma Mongoose pour l'entité Réservation.
+ * @class ReservationSchema
+ * @type {mongoose.Schema}
+ * @property {number} catwayNumber - Le numéro du catway associé à la réservation.
+ * @property {string} clientName - Le nom du client effectuant la réservation.
+ * @property {string} boatName - Le nom du bateau associé à la réservation.
+ * @property {Date} startDate - La date de début de la réservation.
+ * @property {Date} endDate - La date de fin de la réservation.
+ * @property {Date} createdAt - Horodatage de la création du document. Géré automatiquement par `timestamps`.
+ * @property {Date} updatedAt - Horodatage de la dernière mise à jour du document. Géré automatiquement par `timestamps`.
+ */
 const ReservationSchema = new mongoose.Schema(
     {
         catwayNumber: {
@@ -31,4 +49,8 @@ const ReservationSchema = new mongoose.Schema(
     }
 );
 
+/**
+ * Le modèle Mongoose pour une réservation, basé sur ReservationSchema.
+ * @type {mongoose.Model<mongoose.Document>}
+ */
 module.exports = mongoose.model("Reservation", ReservationSchema);
