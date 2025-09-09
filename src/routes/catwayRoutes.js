b315b2735bb10717c8bffe023d80d5ece2a1f179
Leo.Lamarre <leo.lamarre24@gmail.com>
2025-09-09 14:35:26 +0200

    Création de la route PUT /catways/id:/state

--- src/routes/catwayRoutes.js
+++ src/routes/catwayRoutes.js
@@ -3,8 +3,20 @@ const router = express.Router();
 const catwayController = require("../controllers/catwayController");
 const catwayValidator = require("../validators/catwayValidator");
 
-router.post("/", catwayValidator.createCatwayValidator, catwayController.createCatway);
+router.post(
+    "/",
+    catwayValidator.createCatwayValidator,
+    catwayController.createCatway
+);
 router.get("/", catwayController.getAllCatways);
-router.get("/:id", catwayValidator.getCatwayByIdValidator, catwayController.getCatwayById);
-
+router.get(
+    "/:id",
+    catwayValidator.getCatwayByIdValidator,
+    catwayController.getCatwayById
+);
+router.put(
+    "/:id/state",
+    catwayValidator.updateCatwayStateValidator,
+    catwayController.updateCatwayState
+);
 module.exports = router;
