4686a5dc1eb9daa07da78a3d97ea248869f253ad
Leo.Lamarre <leo.lamarre24@gmail.com>
2025-09-11 20:17:50 +0200

    Création du service updateUser

--- src/services/userService.js
+++ src/services/userService.js
@@ -25,14 +25,62 @@ const createUser = async (userData) => {
 
 const getAllUsers = async () => {
     return await User.find().select("-password");
-}
+};
 
 const getUserByEmail = async (email) => {
     return await User.findOne({ email }).select("-password");
-}
+};
+
+const updateUser = async (email, updateData) => {
+    const user = await User.findOne({ email });
+    if (!user) {
+        const error = new Error("Utilisateur non trouvé");
+        error.statusCode = 404;
+        throw error;
+    }
+
+    if (updateData.username) {
+        user.username = updateData.username;
+    }
+
+    if (updateData.email) {
+        const existingUser = await User.findOne({ email: updateData.email });
+        if (
+            existingUser &&
+            existingUser._id.toString() !== user._id.toString()
+        ) {
+            const error = new Error("Email déjà utilisé");
+            error.statusCode = 409;
+            throw error;
+        }
+        user.email = updateData.email;
+    }
+
+    if (updateData.password) {
+        const hashedPassword = await hashPassword(updateData.password);
+        user.password = hashedPassword;
+    }
+
+    if (!updateData.username && !updateData.email && !updateData.password) {
+        return user.toObject({
+            transform: (__, ret) => {
+                delete ret.password;
+                return ret;
+            },
+        });
+    }
+
+    const updatedUser = await user.save();
+
+    const userOBJ = updatedUser.toObject();
+    delete userOBJ.password;
+
+    return userOBJ;
+};
 
 module.exports = {
     createUser,
     getAllUsers,
     getUserByEmail,
+    updateUser,
 };
