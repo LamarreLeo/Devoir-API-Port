b2d6518e71d9a6da8f58422e1f2d42f3795b9d18
Leo.Lamarre <leo.lamarre24@gmail.com>
2025-09-11 14:04:30 +0200

    Création de la route GET /users/:email

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,5 +5,10 @@ const userValidator = require("../validators/userValidator");
 
 router.post("/", userValidator.registerValidator, userController.createUser);
 router.get("/", userController.getAllUsers);
+router.get(
+    "/:email",
+    userValidator.getUserByEmailValidator,
+    userController.getUserByEmail
+);
 
 module.exports = router;
