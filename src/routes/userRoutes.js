4c4615d3d2e77470619102ba72839ef1b51b0001
Leo.Lamarre <leo.lamarre24@gmail.com>
2025-09-12 12:10:26 +0200

    Création de la route DELETE /users/:email

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,16 +4,25 @@ const userController = require("../controllers/userController");
 const userValidator = require("../validators/userValidator");
 
 router.post("/", userValidator.registerValidator, userController.createUser);
+
 router.get("/", userController.getAllUsers);
+
 router.get(
     "/:email",
     userValidator.getUserByEmailValidator,
     userController.getUserByEmail
 );
+
 router.put(
     "/:email",
     userValidator.updateUserValidator,
     userController.updateUser
+);
+
+router.delete(
+    "/:email",
+    userValidator.getUserByEmailValidator,
+    userController.deleteUser
 );
 
 module.exports = router;
