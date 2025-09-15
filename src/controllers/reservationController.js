33b8f5f4329a50d3a228773f8316671a6a6956e6
Leo.Lamarre <leo.lamarre24@gmail.com>
2025-09-15 15:29:33 +0200

    Création du controller createReservation

---
+++ src/controllers/reservationController.js
@@ -0,0 +1,31 @@
+const { validationResult } = require("express-validator");
+const reservationService = require("../services/reservationService");
+
+const createReservation = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const catwayNumber = Number(req.params.id);
+    const reservationData = { catwayNumber, ...req.body };
+
+    try {
+        const reservation = await reservationService.createReservation(
+            reservationData
+        );
+        return res.status(201).json(reservation);
+    } catch (error) {
+        if (error.message === "Catway non trouvé") {
+            return res.status(404).json({ message: error.message });
+        }
+        if (error.message === "Ce catway est déjà réservé sur ces dates") {
+            return res.status(409).json({ message: error.message });
+        }
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports = {
+    createReservation,
+};
