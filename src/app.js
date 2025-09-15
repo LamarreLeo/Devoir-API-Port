626376ba94fdfca96bf2d96f96b0f5c123993827
Leo.Lamarre <leo.lamarre24@gmail.com>
2025-09-15 21:36:09 +0200

    Création est importation de la route PUT /catways/:id/reservations

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const connectDB = require("./config/db");
 const sessionMiddleware = require("./middlewares/sessionMiddleware");
 const userRoutes = require("./routes/userRoutes");
 const catwayRoutes = require("./routes/catwayRoutes");
+const reservationRoutes = require("./routes/reservationRoutes");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -16,6 +17,7 @@ sessionMiddleware(app);
 
 app.use("/api/users", userRoutes);
 app.use("/api/catways", catwayRoutes);
+app.use("/api/catways/:id", reservationRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
---
+++ src/routes/reservationRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const reservationController = require("../controllers/reservationController");
+const reservationValidator = require("../validators/reservationValidator");
+const authMiddleware = require("../middlewares/authMiddleware");
+
+router.use(authMiddleware);
+
+router.post(
+    "/reservations",
+    reservationValidator.createReservationValidator,
+    reservationController.createReservation
+);
+
+module.exports = router;
